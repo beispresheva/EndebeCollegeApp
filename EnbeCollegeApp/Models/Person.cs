7ee2462fc53c2578070bb190cd8d9a45392d5fac
Beis Presheva <beis.presheva@riinvest.net>
2020-05-18 18:43:32 +0200

    Initial second

--- EnbeCollegeApp/Models/Person.cs
+++ EnbeCollegeApp/Models/Person.cs
@@ -7,6 +7,17 @@ namespace EnbeCollegeApp.Models
 {
     public class Person
     {
-        public int Test { get; set; }
+        public string ID { get; set; }
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+        public string Gjinia { get; set; }
+        public string NumriLeternjoftimit { get; set; }
+        public DateTime Datelindja { get; set; }
+        public string Shteti { get; set; }
+        public string Qyteti { get; set; }
+        public string Email { get; set; }
+        public string NumriTelefonit { get; set; }
+        public string Password { get; set; }
+        public string Roli { get; set; }
     }
 }
