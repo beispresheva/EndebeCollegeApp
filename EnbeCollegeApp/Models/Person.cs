9adf2bd6cf26aaa128fd60981b8d6c9eb7b89dea
Beis Presheva <beis.presheva@riinvest.net>
2020-05-18 18:39:16 +0200

    initial test

---
+++ EnbeCollegeApp/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnbeCollegeApp.Models
+{
+    public class Person
+    {
+        public int Test { get; set; }
+    }
+}
---
+++ EnbeCollegeApp/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnbeCollegeApp.Models
+{
+    public class Student : Person
+    {
+        public string EmriPrindit { get; set; }
+        public string MbiemriPrindit { get; set; }
+        public string EmailiPrindit { get; set; }
+        public string NumriTelefonitPrindit { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public int SemestriPare { get; set; }
+        public int SemestriAktual { get; set; }
+    }
+}
