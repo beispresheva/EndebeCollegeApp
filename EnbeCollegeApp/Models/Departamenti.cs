fa193031e90d967373819257c563f71393182923
Endrit Tmava <endrit.tmava@riinvest.net>
2020-05-18 18:49:20 +0200

    klasa departamenti

---
+++ EnbeCollegeApp/Models/Departamenti.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnbeCollegeApp.Models
+{
+    public class Departamenti
+    {
+        //u kriju departamenti
+
+    }
+}
